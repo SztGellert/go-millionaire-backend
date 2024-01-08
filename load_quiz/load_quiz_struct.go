76e4e107514e1808cdbc4f6adc920cb7d72aaeef
SztGellert <sztankgellert@gmail.com>
2024-01-08 02:40:47 +0100

    added language based question objects

--- load_quiz/load_quiz_struct.go
+++ load_quiz/load_quiz_struct.go
@@ -1,10 +1,16 @@
 package load_quiz
 
 type Question struct {
-	Id            int32    `bson:"id" json:"id"`
-	Value         string   `bson:"value" json:"value"`
-	Answers       []string `bson:"answers" json:"answers"`
-	CorrectAnswer string   `bson:"correct_answer" json:"correct_answer"`
-	Difficulty    string   `bson:"difficulty" json:"difficulty"`
-	Topic         string   `bson:"topic" json:"topic"`
+	Id         int32           `bson:"id" json:"id"`
+	En         QuestionDetails `bson:"en" json:"en"`
+	De         QuestionDetails `bson:"de" json:"de"`
+	Hu         QuestionDetails `bson:"hu" json:"hu"`
+	Difficulty string          `bson:"difficulty" json:"difficulty"`
+	Topic      string          `bson:"topic" json:"topic"`
+}
+
+type QuestionDetails struct {
+	Text               string   `bson:"text" json:"text"`
+	Answers            []string `bson:"answers" json:"answers"`
+	CorrectAnswerIndex int32    `bson:"correct_answer_index" json:"correct_answer_index"`
 }
